0df79cb6d5ccd2109a3026c7755555478a0e0516
elsanmurselov <elsanmurselov@gmail.com>
2024-10-16 15:20:35 +0400

    first commit

---
+++ ClientSidee/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+
+
+Console.WriteLine("Client");
+var client = new UdpClient();
+var endPoint = new IPEndPoint(IPAddress.Parse("192.168.0.248"), 27001);
+try
+{
+
+    while (true)
+    {
+
+        Console.Write("Path:");
+        var path = Console.ReadLine();
+
+
+
+        if (Path.GetExtension(path) == ".png" && !string.IsNullOrEmpty(path))
+        {
+            var bytes = new byte[1024];
+            int bytesRead = 0;
+            using (FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+
+
+                do
+                {
+
+                    bytesRead = await fileStream.ReadAsync(bytes, 0, bytes.Length);
+
+                    if (bytesRead > 0)
+                    {
+
+                        client.Send(bytes, bytesRead, endPoint);
+                    }
+
+                } while (bytesRead > 0);
+                var bytes2 = Encoding.UTF8.GetBytes("END");
+                client.Send(bytes2,endPoint);
+
+            }
+            Console.WriteLine("Foto gönderildi ");
+        }
+    }
+
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine(ex.Message);
+}
+
---
+++ NetworkTask2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+Console.WriteLine("Server");
+var endPoint = new IPEndPoint(IPAddress.Parse("192.168.0.248"), 27001);
+var server = new UdpClient(endPoint);
+var remoteEp = new IPEndPoint(IPAddress.Any, 0);
+while (true)
+{
+
+
+    var bytes = server.Receive(ref remoteEp);
+    var directoryPath = $@"{Directory.GetCurrentDirectory()}\{remoteEp.Address}";
+    Directory.CreateDirectory(directoryPath);
+    var filePath = $@"{directoryPath}\{Guid.NewGuid()}.png";
+
+    using (FileStream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+    {
+        Console.WriteLine("Server listen etmeye basladi...");
+
+
+        while (true) { 
+        await fileStream.WriteAsync(bytes, 0, bytes.Length);
+    
+            await fileStream.FlushAsync();
+            bytes = server.Receive(ref remoteEp);
+            if (Encoding.UTF8.GetString(bytes) == "END")
+            {
+                break;
+            }
+
+
+            Console.WriteLine($"file a  {bytes.Length} byte data yazildi");
+        }
+        Console.WriteLine("File data axini sonlandi");
+       
+
+
+    }
+}
